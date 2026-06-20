361cd557e724cf48282c2703d5187fb8785fd7ac
yj35966-ops <yj35966@gmail.com>
2026-06-20 20:24:17 +0530

    string

---
+++ functions.py
@@ -0,0 +1,17 @@
+# find().index(),rfind()
+str1=input("enter main string")
+str2=input("enter substring of main string find")
+x=str1.index(str2)
+print(x)
+
+
+# state=input("enter state:")
+# st=state.strip()
+# if st=="maharashtra":
+#     print("capital:mumbai")
+# elif st=="goa":
+#     print("capital:panji")
+# elif st=="karnataka":
+#     print("capital:bnaglore")
+# else:
+#     print("invalid state")
---
+++ string_slice.py
@@ -0,0 +1,8 @@
+s="python is easy"
+# print(s[2:9])
+# print(s[::2])
+# print(s[::-1])
+# print(s[-1:-5:-1])
+# print(s[-8:-1])
+print(s[10:2:-1])
+# positive slice left to right
